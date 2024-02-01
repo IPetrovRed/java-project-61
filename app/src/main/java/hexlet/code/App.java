40ac6f559db16ae5a38f8d313b114de16433aa2e
IPetrovRed <ipetrovdat@gmail.com>
2024-02-01 18:28:31 +0300

    update App at 01.02.2024

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,8 +2,12 @@ package hexlet.code;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("Please enter the game number and press Enter.\n" +
+                "1 - Greet\n" +
+                "0 - Exit");
         Cli.getName();
+        System.out.println("Welcome to the Brain Games!\n" +
+                "May i have your name?");
 
     }
 }
